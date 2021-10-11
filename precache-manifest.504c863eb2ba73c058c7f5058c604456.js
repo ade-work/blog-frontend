0a9e181718255de838d76b1cb0e43d502bfb1f14
ade-work <adeptgoo@gmail.com>
2021-10-12 01:25:48 +0300

    eab32ec

---
+++ precache-manifest.504c863eb2ba73c058c7f5058c604456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72677afd5c8065aff437",
+    "url": "/css/app.a8c4b6e3.css"
+  },
+  {
+    "revision": "573ff2ced160a3e63a2d44865f3d2fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3cc3879839ce740b267",
+    "url": "/js/about.bdaffcac.js"
+  },
+  {
+    "revision": "72677afd5c8065aff437",
+    "url": "/js/app.ffa00443.js"
+  },
+  {
+    "revision": "20704cc55e4323f487ea",
+    "url": "/js/chunk-vendors.4964226c.js"
+  },
+  {
+    "revision": "5095f18b5f98339dd242ba762f179476",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
